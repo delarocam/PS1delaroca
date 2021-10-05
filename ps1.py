92a741474d6b445574b278f8fa51f3c916b021ac
Matthew De La Roca <mwdelaroca@gmail.com>
2021-10-05 15:30:40 -0700

    part 3 jacks doubling

--- ps1.py
+++ ps1.py
@@ -2,13 +2,17 @@
 
 # creating function to predict bill's 10 year wealth
 
+Bill_interest = 0.05
+
+Bill_principle = 1000
+
 
 def Bill_function(principle, interest):
     total = principle * (1 + interest/1) ** 10
     print("Bill's total wealth is $", round(total, 2))
 
 
-Bill_function(1000, 0.05)
+Bill_function(Bill_principle, Bill_interest)
 
 # Problem 2 part 2
 
@@ -22,4 +26,14 @@ def Bill_double(interest):
     print("It takes", round(years, 2), " years to double Bill's money.")
 
 
-Bill_double(.05)
+Bill_double(Bill_interest)
+
+# problem 2 part 3
+
+Jack_principle = 5000
+
+Jack_interest = .20
+
+# finding out if double
+
+print(70 / (Jack_interest * 100) >= 2 * Jack_principle)
