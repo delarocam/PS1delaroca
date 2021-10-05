c3754f3c6a2aefad2849e469691a38a72dd74db3
Matthew De La Roca <mwdelaroca@gmail.com>
2021-10-04 22:39:52 -0700

    bill's 10 year interest function

--- ps1.py
+++ ps1.py
@@ -1,1 +1,20 @@
 # problem 2 part 1
+
+# creating function to predict bill's 10 year wealth
+
+
+def Bill_function(principle, interest):
+    total = principle * (1 + interest/1) ** 10
+    print("Bill's total wealth is $", round(total, 2))
+
+
+Bill_function(1000, 0.05)
+
+# Problem 2 part 2
+
+# creating function to predict doubling of Bill's Money
+#using rule of 70 for doubling
+
+
+def Bill_double(interest):
+    years = 70 / interest
